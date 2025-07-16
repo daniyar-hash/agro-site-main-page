a94355a2827d663c7d61174d70d773e144f108d6
Daniyar <Daniyar@DESKTOP-2N3V8QS>
2025-07-16 19:09:55 +0600

    add video in card-product

--- js/script.js
+++ js/script.js
@@ -2,7 +2,15 @@
   const header__inner = document.querySelector('.header__inner');  
   const header__logo = document.querySelector('.header__logo');
   const header__container = document.querySelector('.header__container');
+  const modalVideo = document.querySelector('.open-modal-video');
+  const modal_section = document.querySelector('.modal');
 
+
+  modalVideo.addEventListener("click", (e)=>{
+      modal_section.classList.toggle('modal-show');
+      modal_section.style.display = "block";
+      modal_section.querySelector('#video').play();
+  })
 
   function updateHeaderClass() {
     if (window.scrollY > 100) {
@@ -26,11 +34,6 @@
 
   // Проверка при загрузке страницы
    window.addEventListener('load', updateHeaderClass);
-
-
-
-
-
 
   // Проверка при скролле
   window.addEventListener('scroll', updateHeaderClass);
